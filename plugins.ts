7027e6bf5e0a61c6014aa3c196c6ae6dffc41de4
Andrew Cleland <andrew.cleland@mangoesmapping.com.au>
2024-08-01 09:54:02 +1000

    Use sha docker tags

--- plugins.ts
+++ plugins.ts
@@ -5,7 +5,7 @@ import { TerriaPluginModule } from "terriajs-plugin-api";
  */
 const plugins: () => Promise<TerriaPluginModule>[] = () => [
   // Add plugin imports. Example:
-  import("terriajs-mvt-search")
+  //import("terriajs-mvt-search")
 ];
 
 export default plugins;
